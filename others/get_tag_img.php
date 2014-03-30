1ff4519436469459990125e0b56024e4b2619d13
genya <ishida.genya.ex@gmail.com>
2014-03-30 23:52:13 +0900

    タグの画像を取得するプログラム
    GETでidを取得して、Tagテーブルに対してid検索する

---
+++ others/get_tag_img.php
@@ -0,0 +1,27 @@
+<?php
+$dsn = 'mysql:dbname=mydb;host=127.0.0.1';
+$user = 'root';
+$pswd = '211293g2';
+$options = array(
+  PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/my.cnf',
+); 
+ 
+$id = intval($_GET['id']);
+
+try{
+  $dbh = new PDO($dsn, $user, $pswd, $options); 
+  $sql = "SELECT * FROM Tag WHERE Tag_ID = ".$id;
+  $stmt = $dbh->query($sql);
+  if($stmt!=null){
+    $result = $stmt->fetchObject();//(PDO::FETCH_ARRAY);
+    header("Content-Type: image/png");//pngで固定しているので注意
+    echo $result->Icon_picture;
+  }
+}
+catch (PDOEXception $e){
+  die();
+}
+$dbh=null;
+?>
+
+
