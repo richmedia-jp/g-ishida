278c36bdbf893c64d461c5d69ce06c357d9271c2
genya <ishida.genya.ex@gmail.com>
2014-03-30 23:49:44 +0900

    タグ検索機能実装、検索結果に美容室の画像を表示

--- user_site/salon_search_result.php
+++ user_site/salon_search_result.php
@@ -1,5 +1,26 @@
 <?php
-  require('header.php');
+require('header.php');
+
+
+$dsn = 'mysql:dbname=mydb;host=127.0.0.1';
+$user = 'root';
+$pswd = '211293g2';
+$options = array(
+PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/my.cnf',
+); 
+
+try{
+  $dbh_t = new PDO($dsn,$user, $pswd, $options);
+  $sql_t = "SELECT * FROM Tag WHERE Tag_ID >= 2 AND Tag_ID <= 6";
+  $stmt_t = $dbh_t->query($sql_t);
+
+}catch (PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
+$dbh_t=null;
+
+
 ?>
 
 
@@ -7,9 +28,9 @@
   <div id="contents">
 
     <div id="salon_search_result">
-     
+      <form action="salon_search_result.php" method="post">
       <div id="salon_search_item">
-        <form action="salon_search_result.php" method="post">
+        <div id="salon_search_ward">
           <table>
             <tr><td>エリア</td><td>キーワード</td><td></td></tr>
             <tr>
@@ -18,21 +39,37 @@
               <td><input type="submit" value="検索"</td>
             </tr>
           </table>
-        </form>
-      </div>
+        </div>
 
+        <div id="salon_search_tag">
+          <table>
+<?php
+          $ti=0;
+          while($result_t=$stmt_t->fetch(PDO::FETCH_ASSOC)){
+            if($ti%3==0){echo '          <tr>';}
+            echo '<td><img src="../get_tag_img.php?id='.$result_t['Tag_ID'].'"></td>';
+            echo '<td>'.$result_t['Tag_name'].'</td>';
+            echo '<td><input type="checkbox" name="tag[]" class="check_form" value="'.$result_t['Tag_ID'].'"';
+            for($i=0;$i <count($_POST["tag"]); $i++){
+              if($result_t['Tag_ID']==$_POST["tag"][$i]){
+                echo ' checked';
+              }
+            }
+            echo'></td>';
+            if($ti%3==2){echo "</tr>\n";}
+            $ti++;
+          }
+          if($ti%3!=0){echo "</tr>\n";}
+?>
+          </table>
+        </div>
+
+      </div>
+      </form>
 
       <div id="salon_search_list">
+
 <?php
-
-  $dsn = 'mysql:dbname=mydb;host=127.0.0.1';
-  $user = 'root';
-  $pswd = '211293g2';
-  $options = array(
-  PDO::MYSQL_ATTR_READ_DEFAULT_FILE => '/etc/my.cnf',
-  ); 
-
-
 try{
   $dbh = new PDO($dsn, $user, $pswd, $options); 
 
@@ -41,14 +78,20 @@ try{
     $sql = "SELECT * FROM Salon WHERE Prefecture_ID = ".$_GET['local_area'];
   }
   else{//エリア・キーワード検索（AND検索）
+
     $area="";
     $keyword="";
  
     if($_POST['area']!=""){$area=htmlspecialchars($_POST['area']);}
     if($_POST['keyword']!=""){$keyword=htmlspecialchars($_POST['keyword']);}
 
-    if($area != null){//エリア検索窓に入力があるとき
-      //-- 都道府県の検索を先にしておく -------------//
+
+//新規で書いた処理
+    $area_sql="";
+    $key_sql="";
+    $tag_sql="";
+    if($area!=null){
+    //-- 都道府県はIDしかわからないのe都道府県で該当するならそれをOR条件に入れるために都道府県名を取得する ----//
       $pref_ID = 0;
       $sql_p = "SELECT * FROM Prefecture WHERE Prefecture_name LIKE '%".$area."%'";
       $stmt_p = $dbh->query($sql_p);
@@ -56,50 +99,157 @@ try{
         $result_p = $stmt_p->fetch(PDO::FETCH_ASSOC);
         $pref_ID = $result_p['Prefecture_ID'];
       }
-      //----------------------------------------------//
+    //----------------------------------------------------------------------------------------------------------//
+    $area_sql="(Address1 LIKE '%".$area."%' OR Address2 LIKE '%".$area."%'";
+    if($pref_ID!=0){
+      $area_sql = $area_sql.' OR Prefecture_ID='.$pref_ID.')';
+    }
+    else{$area_sql = $area_sql.')';}
+    }
 
-      
-      if($keyword != null){//キーワード検索窓に入力があるとき
-        if($pref_ID != 0){//エリア検索ワードが都道府県名に引っかかるとき（OR条件に足す）
-          $sql = "SELECT * FROM Salon WHERE (Prefecture_ID = ".$pref_ID." OR Address1 LIKE '%".$area."%' OR Address2 LIKE '%".$area."%') AND (Introduction_title LIKE '%".$keyword."%' OR Introduction_text LIKE '%".$keyword."%')";
+    if($keyword != null){
+      $key_sql = "(Introduction_title LIKE '%".$keyword."%' OR Introduction_text LIKE '%".$keyword."%')";
+    }
+    
+    if(isset($_POST["tag"])){//保留　中間テーブルの存在がむずい
+      $tagbox=array(0);//23456
+      $sql_t='';
+      for($i=0; $i < count($_POST["tag"]); $i++) {
+        if($i==0){
+          $sql_t = "(Tag_Tag_ID = ".$_POST["tag"][$i];
         }
-        else{//エリア検索ワードが都道府県名に引っかからない時
-          $sql = "SELECT * FROM Salon WHERE (Address1 LIKE '%".$area."%' OR Address2 LIKE '%".$area."%') AND (Introduction_title LIKE '%".$keyword."%' OR Introduction_text LIKE '%".$keyword."%')";
+        else{
+          $sql_t = $sql_t." OR Tag_Tag_ID = ".$_POST["tag"][$i];
         }
       }
-      else {//キーワード検索窓には入力がないとき
-        if($pref_ID != 0){//エリア検索ワードが都道府県名に引っかかるとき（OR条件に足す）
-          $sql = "SELECT * FROM Salon WHERE Prefecture_ID = ".$pref_ID." OR Address1 LIKE '%".$area."%' OR Address2 LIKE '%".$area."%'";
+
+      $sql_t = "SELECT DISTINCT Salon_Salon_ID FROM Salon_has_Tag WHERE ".$sql_t.")";
+      $stmt_st = $dbh->query($sql_t);
+      if($stmt_st==""){print_r($dbh->errorInfo());}
+      $sidbox=array();
+      while($result_st=$stmt_st->fetch(PDO::FETCH_ASSOC)){
+        //echo $result_st['Salon_Salon_ID']."\n";
+        array_push($sidbox, $result_st['Salon_Salon_ID']);
+      }
+
+      if(count($sidbox) > 0){//条件のタグを一部でもみたすデータが存在したら
+        $tidbox=array();
+
+        for($i=0; $i < count($sidbox); $i++){
+          //候補のIDと一致するデータを全部出す//$shastboxに入れる
+          $sql_t = "SELECT * FROM Salon_has_Tag WHERE Salon_Salon_ID = ".$sidbox[$i];
+          $stmt_st = $dbh->query($sql_t);
+          if($stmt_st ==""){print_r($dbh->erroIfo());}
+          $shastbox=array();
+          while($result_st=$stmt_st->fetch(PDO::FETCH_ASSOC)){
+            array_push($shastbox,$result_st['Tag_Tag_ID']);
+          }
+
+          //候補のIDがもってるチェック済み項目($shastbox)の中に、検索条件のチェック($_POST["tag"])がすべてあるかを調べる
+          $all_tagcheck_flag=false;
+          for($j=0; $j<count($_POST["tag"]); $j++){
+            $tagcheck_flag=false;
+            for($k=0; $k<count($shastbox);$k++){
+              if($_POST["tag"][$j] == $shastbox[$k]){$tagcheck_flag=true;break;}//該当するタグをもっていたら
+            }
+            if($tagcheck_flag==true){//該当するタグがあった
+              $all_tagcheck_flag=true;
+            }
+            else{$all_tagcheck_flag=false;break;}
+          }
+          if($all_tagcheck_flag == true){array_push($tidbox, $sidbox[$i]);}//最後まで#all_tagcheck_flag==trueならここに入る
+
         }
-        else{//エリア検索ワードが都道府県名に引っかからない時
-          $sql = "SELECT * FROM Salon WHERE Address1 LIKE '%".$area."%' OR Address2 LIKE '%".$area."%'";
+        //print_r($tidbox);//すべての条件のチェックをみたしていたIDを表示
+
+        if(count($tidbox)>0){
+          for($i=0; $i < count($tidbox); $i++){
+            if($i==0){$tag_sql="(Salon_ID = ".$tidbox[$i];}
+            else{$tag_sql=$tag_sql." OR Salon_ID = ".$tidbox[$i];}
+          }
+          $tag_sql=$tag_sql.")";
         }
+        else{$tag_sql="(Salon_ID < 1)";}//タグの条件に当てはまるデータがなかった
+        
+      }
+      else{
+        $tag_sql="(Salon_ID < 1)";//タグの条件にあてはまるデータがなかったため。絶対にひっかからない文言にする
       }
 
     }
-    else if($keyword != null){//エリア検索窓には入力がなく、キーワード検索窓に入力がある時//キーワードでエリア検索も兼ねても良い気がする。
-      $sql = "SELECT * FROM Salon WHERE Introduction_title LIKE '%".$keyword."%' OR Introduction_text LIKE '%".$keyword."%'";
+
+
+
+    if($area_sql==""){//エリアなし
+
+      if($key_sql==""){//キーワードなし
+
+        if($tag_sql==""){//エリアなし、キーワードなし、タグチェックなし
+          $sql = '';
+        }
+        else{//タグのみあり
+          $sql = 'SELECT * FROM Salon WHERE '.$tag_sql;
+        }
+
+      }
+      else{//キーワードあり
+        $sql = "SELECT * FROM Salon WHERE ".$key_sql;
+        if($tag_sql != ""){//キーワードあり、タグあり
+          $sql = $sql." AND ".$tag_sql;
+        }
+        else{}//キーワードあり、タグ無し
+      }
+
     }
-    else{$sql='';}//検索窓が共に空のとき
+    else{//エリアあり
+      $sql = "SELECT * FROM Salon WHERE ".$area_sql;
+      if($key_sql==""){//キーワードなし
+
+        if($tag_sql!=""){//エリアあり、キーワードなし、タグあり
+         $sql =$sql." AND ".$tag_sql;
+        }
+        else{}//エリアあり、キーワードなし、タグ無し
+
+      }
+      else{//キーワードあり
+        $sql = $sql." AND  ".$key_sql;
+        if($tag_sql != ""){//エリアあり、キーワードあり、タグ有り
+          $sql = $sql." AND ".$tag_sql;
+        }
+        else{}//エリア有り、キーワードあり、タグ無し
+      }
+
+    }
 
   }
-
+ 
 //------------------------------------------------------------------------------------------------------------//
 
 //--- 検索開始↓ ----------------------------------------------------------------------------------------------------//
   if($sql==''){//検索窓が空のとき
-    echo '<p>エリア、キーワードを入力して検索ボタンを押して下さい</p><br>';
+    echo '<p>条件を指定してから検索ボタンを押して下さい</p><br>';
   }
   else{
 
     $stmt = $dbh->query($sql);
-    if($stmt==""){print_r($dbh->errorInfo());}
+    if($stmt==""){echo $sql;print_r($dbh->errorInfo());}
     else{
+      $no_result_flag=1;//該当件数が0かどうかを判定するフラグ。下のwhile文に入れば、0として該当件数アリと判断
       while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        //画像のIDの取得-------------//
+        $sql_pic = "SELECT * FROM Salon_has_SalonPicture WHERE Salon_Salon_ID = ".$result['Salon_ID'];
+        $stmt_pic = $dbh->query($sql_pic);
+        $Picture_ID=4;//デフォルト値
+        while($result_pic = $stmt_pic->fetch(PDO::FETCH_ASSOC)){
+          $Picture_ID=$result_pic['SalonPicture_Picture_ID'];
+          break;//代表値ひとつだけでいいので1回でbreak
+        }
+        //-------------------------///
+
+        $no_result_flag=0;
         echo '        <div class="salon_search_list_box">'."\n";
         echo '          <div class="salon_name">'.'<a href=salon_detail.php?id='.$result['Salon_ID'].'>'.$result['Salon_name']."</a></div>\n";
- //     echo '          <a href="salon_detail.php?id='.$result['Salon_ID'].'"><img src="img_get.php?id='.$result['Picture_ID'].'" /></a>'."\n";/Picture_IDは複数あるから、そのうちを一つを取ってくるような処理にしないとマズイ。そもそもこのwhileが同じIDのものを複数発見する可能性がある気がする /
-        echo '          <a href="salon_detail.php?id='.$result['Salon_ID'].'"><img src="img_get.php?id=4" alt="美容室のイメージ" /></a>'."\n";
+        echo '          <a href="salon_detail.php?id='.$result['Salon_ID'].'"><img src="../get_salon_img.php?id='.$Picture_ID.'" alt="美容室のイメージ" /></a>'."\n";
         echo '          <div class="introduction_text">'.$result['Introduction_text'].'</div>'."\n";
         echo '          <div class="basic_info">'."\n";
         echo "            <table>\n";
@@ -121,10 +271,11 @@ try{
         echo "          </div>\n";
         echo "        </div>\n";
       }
-      if(count($result)==null){//検索した結果、該当データがなかったとき//この条件はよくなさそう。
-        echo '<p>該当する美容室はありませんでした。エリア、キーワードを変えて検索してみてください</p><br>';
+      if ($no_result_flag==1){
+        echo "<p>該当するデータが見つかりませんでした。条件を変えて検索してみてください。</p><br>\n";
       }
-    }
+      
+   }
 
   }
 //-----------------------------------------------------------------------------------------------------------------------//
