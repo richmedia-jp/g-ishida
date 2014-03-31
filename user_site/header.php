65820d839a2d8e21bd08fcb70313f74a8b4e263b
genya <ishida.genya.ex@gmail.com>
2014-03-31 18:00:50 +0900

    ユーザ向け検索サイトのヘッダファイル
    すべてのページで呼ばれる

---
+++ user_site/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel=stylesheet type="text/css" href="style.css">
+<title>美容室・美容師検索サイト</title>
+</head>
+<body>
+
+  <div id="header">
+    <div id="navmenu">
+      <div id="site_logo">
+        <a href="index.php">検索サイト</a>
+      </div>
+
+      <ul>
+        <li><a href="salon_search_top.php">美容室検索TOP</a></li>
+        <li><a href="stylist_search_top.php">美容師検索TOP</a></li>
+      </ul>
+
+    </div>
+
+  </div>
+
+ 
